8637cdae92e63d6a46273d6878e3aeda0958a24b
Maksim Sisov <msisov@igalia.com>
2017-05-04 16:34:52 +0300

    [ozone/wayland] Fix Showing/Hiding, Placing and Events for Menu Windows (#73)
    This adaptation makes it possible to show/hide and place menu windows
    properly in regards to their parent surface. What is more, this patch
    fixes event locations sent to the client from wayland.
    Conflicts:
    	ui/ozone/platform/wayland/wayland_window.cc
    
    fixup! [Wayland] Fix Showing/Hiding, Placing and Events for Menu Windows (#73)
    
    This resolves crash when a user requests to open a submenu window
    
    fixup! [Wayland] Fix Showing/Hiding, Placing and Events for Menu Windows (#73)
    
    Typo fix
    
    fixup! [Wayland] Fix Showing/Hiding, Placing and Events for Menu Windows (#73)
    
    fixup! [ozone/wayland] Fix Showing/Hiding, Placing and Events for Menu Windows (#73)
    
    Fix crash when menus are destroyed, but aura is still trying to hide them.
    It may happen when wayland decides to destroy the menu (wrong serial number, for example).
    The problem here is that we set a child window pointer to the parent, but never remove it.
    
    fixup! [ozone/wayland] Fix Showing/Hiding, Placing and Events for Menu Windows
    
    Ensure that a pointer to child window is invalidated as soon as xdg_popup_ is
    destroyed. This fixes a random crash, when the wrong pointer to a child
    deletes not the top most popup window, which results in a dismiss signal from
    wayland and all windows are being closed.
    
    fixup! [ozone/wayland] Fix Showing/Hiding, Placing and Events for Menu Windows (#73)
    
    This patch makes opening submenus on mouse hover to work. Basically, creating
    popups requires serials, which correspond to grab events (those
    are touch, key press or button press events). As long as other
    events' serials are not used by us, don't save such serials that
    correspond to Enter/Leave and etc.
    
    fixup! [ozone/wayland] Fix Showing/Hiding, Placing and Events for Menu Windows
    
    Remove setting serial from WaylandKeyboard::Modifiers() as well.
    
    fixup! [ozone/wayland] Fix Showing/Hiding, Placing and Events for Menu Windows
    
    Release buffer as soon as the popups are destroyed.
    
    fixup! [ozone/wayland] Fix Showing/Hiding, Placing and Events for Menu Windows (#73)
    
    Add nullcheck
    
    fixup! [ozone/wayland] Fix Showing/Hiding, Placing and Events for Menu Windows (#73)
    
    Minor: No need to use ::get to null-check unique_ptr instances.
    
    Issue #266
    
    fixup! [ozone/wayland] Fix Showing/Hiding, Placing and Events for Menu Windows (#73)
    
    Pass parent WaylandWindow instead of wl_surface to avoid searching for a
    WaylandWindow by wl_surface. This is an effort to prepare upstreaming
    the local changes.
    
    fixup! [ozone/wayland] Fix Showing/Hiding, Placing and Events for Menu Windows (#73)
    
    Driven by base::MakeUnique -> std::make_unique.
    
    TBR=msisov@igalia.com
    
    Issue #266

--- ui/ozone/platform/wayland/wayland_keyboard.cc
+++ ui/ozone/platform/wayland/wayland_keyboard.cc
@@ -70,14 +70,16 @@ void WaylandKeyboard::Enter(void* data,
                             uint32_t serial,
                             wl_surface* surface,
                             wl_array* keys) {
-  WaylandWindow::FromSurface(surface)->set_keyboard_focus(true);
+  if (surface)
+    WaylandWindow::FromSurface(surface)->set_keyboard_focus(true);
 }
 
 void WaylandKeyboard::Leave(void* data,
                             wl_keyboard* obj,
                             uint32_t serial,
                             wl_surface* surface) {
-  WaylandWindow::FromSurface(surface)->set_keyboard_focus(false);
+  if (surface)
+    WaylandWindow::FromSurface(surface)->set_keyboard_focus(false);
 
   WaylandKeyboard* keyboard = static_cast<WaylandKeyboard*>(data);
 
