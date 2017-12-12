0753db7c192ea93533949509282d6b90354c8c1f
Maksim Sisov <msisov@igalia.com>
2017-12-12 15:38:17 +0200

    [TEMP] Revert "mus: Do not host viz by default."
    This reverts commit 418217886ac8dd904b0ba1e53ed42d34d6ae8cff.
    
    Host viz by default before we get a solution, who should host
    viz in external window mode if not mus.
    
    Issue: #337

--- chrome/app/chrome_main.cc
+++ chrome/app/chrome_main.cc
@@ -114,6 +114,9 @@ int ChromeMain(int argc, const char** argv) {
   if (command_line->HasSwitch(switches::kMus)) {
     params.create_discardable_memory = true;
     params.env_mode = aura::Env::Mode::MUS;
+    // TODO(786453): Remove when mus no longer needs to host viz.
+    base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
+        switches::kMus, switches::kMusHostVizValue);
   }
 #if defined(OS_CHROMEOS)
   if (command_line->HasSwitch(switches::kMash)) {
