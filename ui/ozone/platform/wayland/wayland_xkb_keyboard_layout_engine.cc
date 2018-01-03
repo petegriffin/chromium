f5a33755e0435e8d34f6ea44a06c5377228cf745
Antonio Gomes <tonikitoo@igalia.com>
2018-01-03 09:39:09 -0400

    Add a mock impl XKBWaylandKeyboardLayoutEngine
    In order to allow existing unittests (namely WaylandKeyboardTests)
    and future ones to pass with XKB enabled, we need a specialization of
    XkbWaylandKeyboardLayoutEngine for tests.
    Following the logic used throughout the Wayland unittests' machinery,
    it makes sense to mock a Xkb-capable KeyboardLayoutEngine, rather than
    using an actual implementation.
    
    This CL adds a mock implementation of XkbWaylandKeyboardLayoutEngine to
    be used by Wayland unittests.
    
    For this, it turns XkbWaylandKeyboardLayoutEngine into a pure virtual
    class, which is implemented by XkbWaylandKeyboardLayoutEngineImpl
    (for regular ozone/wayland execution, e.g. chrome/wayland), and
    adds a sibling MockXkbWaylandKeyboardLayoutEngine derived class to
    be used for unittests.
    
    BUG=578890
    
    Change-Id: Ie98f0d5bcf5b45c86812c4fab7dc63e1ffdc0890

--- ui/ozone/platform/wayland/wayland_xkb_keyboard_layout_engine.cc
+++ ui/ozone/platform/wayland/wayland_xkb_keyboard_layout_engine.cc
@@ -9,11 +9,11 @@
 
 namespace ui {
 
-WaylandXkbKeyboardLayoutEngine::WaylandXkbKeyboardLayoutEngine(
+WaylandXkbKeyboardLayoutEngineImpl::WaylandXkbKeyboardLayoutEngineImpl(
     const XkbKeyCodeConverter& converter)
-    : XkbKeyboardLayoutEngine(converter) {}
+    : WaylandXkbKeyboardLayoutEngine(converter) {}
 
-void WaylandXkbKeyboardLayoutEngine::SetKeymap(xkb_keymap* keymap) {
+void WaylandXkbKeyboardLayoutEngineImpl::SetKeymap(xkb_keymap* keymap) {
   XkbKeyboardLayoutEngine::SetKeymap(keymap);
 
   xkb_mod_indexes_.control =
@@ -23,10 +23,11 @@ void WaylandXkbKeyboardLayoutEngine::SetKeymap(xkb_keymap* keymap) {
   xkb_mod_indexes_.caps = xkb_keymap_mod_get_index(keymap, XKB_MOD_NAME_CAPS);
 }
 
-void WaylandXkbKeyboardLayoutEngine::UpdateModifiers(uint32_t depressed_mods,
-                                                     uint32_t latched_mods,
-                                                     uint32_t locked_mods,
-                                                     uint32_t group) {
+void WaylandXkbKeyboardLayoutEngineImpl::UpdateModifiers(
+    uint32_t depressed_mods,
+    uint32_t latched_mods,
+    uint32_t locked_mods,
+    uint32_t group) {
   xkb_state_update_mask(xkb_state_.get(), depressed_mods, latched_mods,
                         locked_mods, 0, 0, group);
 
@@ -52,6 +53,11 @@ void WaylandXkbKeyboardLayoutEngine::UpdateModifiers(uint32_t depressed_mods,
     event_modifiers_->SetModifierLock(MODIFIER_CAPS_LOCK, true);
   else
     event_modifiers_->SetModifierLock(MODIFIER_CAPS_LOCK, false);
+}
+
+void WaylandXkbKeyboardLayoutEngineImpl::SetEventModifiers(
+    EventModifiers* event_modifiers) {
+  event_modifiers_ = event_modifiers;
 }
 
 }  // namespace ui
